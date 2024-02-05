b637f2b96ae5f867ecef2b33c89d94b536ed3a9d
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2024-02-05 13:02:41 +0530

    bitwise operator

---
+++ bitwiseOpr2.java
@@ -0,0 +1,9 @@
+public class bitwiseOpr2 {
+    public static void main(String[] args)
+    {
+        int a =100;
+        int b=2;
+        System.out.println(a<<b);
+        System.out.println(a>>b);
+    } 
+}
