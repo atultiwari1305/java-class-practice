a659b1a0e2e8026775f03027e278b81718326b3b
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2024-04-02 12:31:11 +0530

    propagation of exception

---
+++ ExceptionPropagation.java
@@ -0,0 +1,21 @@
+public class ExceptionPropagation {
+    void m(){
+        int data = 50/0;
+    }
+    void n(){
+        m();
+    }
+    void p(){
+        try{
+            n();
+        }
+        catch(ArithmeticException e){
+            System.out.println("Exception Handled");
+        }
+    }
+    public static void main(String[] args){
+        ExceptionPropagation obj = new ExceptionPropagation();
+        obj.p();
+        System.out.println("Normal Flow");
+    }
+}
